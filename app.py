e75b353ef597d0908c221dafa8ead1dfbf62e17f
ronizin <ronizinho09@uni9.edu.br>
2025-09-28 12:08:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,29 @@ from flask import Flask, render_template, request, flash, redirect
 
 app = Flask(__name__)  
 
-@app.route("/", endpoint="inicial") 
-def home(): 
-    return render_template('index.html') 
+@app.route("/home")
+def home():
+    return render_template("index.html")
 
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
 
+@app.route("/cuidador")
+def cuidador():
+    return render_template("cadas_cuidador.html")
+
+@app.route("/ajuda")
+def ajuda():
+    return render_template("ajuda.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 @app.route("/cadastro")
-def adicao():
-    return render_template("cadas.html")
-
-
+def cadastro():
+    return render_template("cadastro")
 
 if __name__ == '__main__': 
     app.run(debug=True)
